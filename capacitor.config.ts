c67f7c1c98ae97032de60addf546d7b65c8f2bf2
Mekhla <sharma.mekhla1993@gmail.com>
2024-07-11 09:49:50 +0530

    Initial commit
    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sample-angular-ionic-cordova',
+  webDir: 'www'
+};
+
+export default config;
